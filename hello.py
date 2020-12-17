50752756cefd2fedb11baf8c245c9fc192d54a3e
pyw0214 <yongwookp@gmail.com>
2020-12-17 20:55:09 +0900

    updated_20201217

--- hello.py
+++
@@ -1,6 +0,0 @@
-age = "18"
-
-print(type(age))
-n_age = int(age)
-print(type(n_age))
-print(n_age)
---
+++ main.py
@@ -0,0 +1,17 @@
+import requests
+from bs4 import BeautifulSoup
+
+indeed_result = requests.get(
+    "https://www.indeed.com/jobs?as_and=python&limit=50")
+
+indeed_soup = BeautifulSoup(indeed_result.text, "html.parser")
+
+pagination = indeed_soup.find("div", {"class": "pagination"})
+
+pages = pagination.find_all("a")
+
+spans = []
+for page in pages:
+    spans.append(page.find("span"))
+
+print(spans[:-1])
