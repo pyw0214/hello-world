511027252aa978e6fc6baf46abf4478d609a08f6
pyw0214 <yongwookp@gmail.com>
2020-12-21 20:41:46 +0900

    updated_20201221

--- indeed.py
+++ indeed.py
@@ -12,27 +12,40 @@ def extract_indeed_pages():
     links = pagination.find_all("a")
 
     pages = []
-    for link in links[:-1]:
-        pages.append(int(link.find("span").string))
+    for page in links[:-1]:
+        pages.append(int(page.find("span").string))
     max_page = pages[-1]
     return max_page
 
 
+def extract_job(html):
+    title = html.find("h2", {"class": "title"}).find("a")["title"]
+    company = html.find("span", {"class": "company"})
+    company_anchor = company.find("a")
+    if company_anchor is not None:
+        company = str(company_anchor.string)
+    else:
+        company = str(company.string)
+    company = company.strip()
+    location = html.find("div", {"class": "recJobLoc"})["data-rc-loc"]
+    job_id = html["data-jk"]
+    return {
+        'title': title,
+        'company': company,
+        'location': location,
+        "link": f"https://www.indeed.com/jobs?as_and=python&vjk={job_id}"
+    }
+
+
 def extract_indeed_jobs(last_page):
     jobs = []
-    #for page in range(last_page):
-    result = requests.get(f"{URL}&start = {0*LIMIT}")
-    soup = BeautifulSoup(result.text, "html.parser")
-    results = soup.find_all("div", {"class": "jobsearch-SerpJobCard"})
-    for result in results:
-        title = result.find("h2", {"class": "title"}).find("a")["title"]
-        company = result.find("span", {"class": "company"})
-        company_anchor = company.find("a")
-        if company_anchor is not None:
-            company = str(company_anchor.string)
-        else:
-            company = str(company.string)
-        company = company.strip()
-        print(title, company)
+    for page in range(last_page):
+        print(f"Scrapping page {page}")
+        result = requests.get(f"{URL}&start = {page*LIMIT}")
+        soup = BeautifulSoup(result.text, "html.parser")
+        results = soup.find_all("div", {"class": "jobsearch-SerpJobCard"})
+        for result in results:
+            job = extract_job(result)
+            jobs.append(job)
 
     return jobs
