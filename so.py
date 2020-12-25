249b8cf1d2888ea31e893088881d31aa66dd0550
pyw0214 <yongwookp@gmail.com>
2020-12-25 20:08:05 +0900

    updated_20201225

--- so.py
+++ so.py
@@ -8,10 +8,32 @@ def get_last_page():
     result = requests.get(URL)
     soup = BeautifulSoup(result.text, "html.parser")
     pages = soup.find("div", {"class": "s-pagination"}).find_all("a")
+    last_page = pages[-2].get_text(strip=True)
+    return int(last_page)
 
-    print(pages)
+
+def extract_job(html):
+    title = html.find("h2", {"class": "mb4"}).find("a")["title"]
+    company, location = html.find("h3", {
+        "class": "fc-black-700"
+    }).find_all("span", recursive=False)
+    print(company.get_text(strip=True), location.get_text(strip=True))
+    return {'title': title}
+
+
+def extract_jobs(last_page):
+    jobs = []
+    for page in range(last_page):
+        result = requests.get(f"{URL}&pg={page + 1}")
+        soup = BeautifulSoup(result.text, "html.parser")
+        results = soup.find_all("div", {"class": "-job"})
+        for result in results:
+            job = extract_job(result)
+            jobs.append(job)
+    return jobs
 
 
 def get_jobs():
     last_page = get_last_page()
-    return []
+    jobs = extract_jobs(last_page)
+    return jobs
