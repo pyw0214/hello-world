d944bff37a54d5a64e5af1f8126b9cd42db88e2c
pyw0214 <yongwookp@gmail.com>
2021-01-01 17:18:08 +0900

    updated_0101

--- helloworld.py
+++ helloworld.py
@@ -1,1 +1,8 @@
-print("hello world")
+def plus(*args):
+    result = 0
+    for number in args:
+        result += number
+    print(result)
+
+
+plus(1, 2, 1, 1,1,1,2,3,1,2, 6, 7, 8, 5, 4, 3, 3, 2, 1, 1)
