445e314c6db6a11d62dbc5faa6e36eb7013d42ba
pyw0214 <yongwookp@gmail.com>
2021-01-02 20:25:45 +0900

    0102

--- helloworld.py
+++ helloworld.py
@@ -9,18 +9,37 @@
 
 #Object Oriented Programming
 class Car():   #blueprint
-    wheels = 4
-    doors = 4
-    windows = 4
-    seats = 4
 
-    def start(self):   #method  = a function in class
-        print(self.color)
-        print("I started")
 
-porsche = Car()
-porsche.color = "Red Sexy Red"
-porsche.start()
+    def __init__(self, **kwargs):
+        print(kwargs.get)
+        self.wheels = 4
+        self.doors = 4
+        self.windows = 4
+        self.seats = 4 
+        self.color = kwargs.get("color", "black")
+        self.price = kwargs.get("price", "$20")
+
+    def __str__(self):
+        return f"Car with {self.wheels} wheels"
+
+
+
+class Convertible(Car):   #blueprint
+
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs) 
+        self.time = kwargs.get("time", 10)
+
+    def take_off(self):
+        return "taking off"
+
+    def __str__(self):
+        return f"Car with no roof"
+
+
+porsche = Convertible(color = "green", price = "$40")
+print(porsche.color)
 
 
 # Porsche = Car()   #instance
