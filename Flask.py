b664dde52b5fa1184b8b14a3fcd94abf7bf94a29
pyw0214 <yongwookp@gmail.com>
2021-01-03 18:25:51 +0900

    updated_210103

--- Flask.py
+++ Flask.py
@@ -1,4 +1,6 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect
+from flask.templating import render_template
+from so import get_jobs
 
 app = Flask("SuperScrapper")
 
@@ -6,6 +8,20 @@ app = Flask("SuperScrapper")
 @app.route("/")
 def home():
     return render_template("potato.html")
+
+
+@app.route("/report")
+def report():
+    word = request.args.get('word')
+    if word:
+        word = word.lower()
+        jobs = get_jobs(word)
+        print(jobs)
+    else:
+        return redirect("/")
+
+    return render_template("report.html",
+                           searchingBy=word)
 
 
 app.run()
--- so.py
+++ so.py
@@ -1,11 +1,9 @@
 import requests
 from bs4 import BeautifulSoup
 
-URL = f"https://stackoverflow.com/jobs?q=python"
 
-
-def get_last_page():
-    result = requests.get(URL)
+def get_last_page(url):
+    result = requests.get(url)
     soup = BeautifulSoup(result.text, "html.parser")
     pages = soup.find("div", {"class": "s-pagination"}).find_all("a")
     last_page = pages[-2].get_text(strip=True)
@@ -28,11 +26,11 @@ def extract_job(html):
     }
 
 
-def extract_jobs(last_page):
+def extract_jobs(last_page, url):
     jobs = []
     for page in range(last_page):
         print(f"Scrapping SO: Page: {page}")
-        result = requests.get(f"{URL}&pg={page + 1}")
+        result = requests.get(f"{url}&pg={page + 1}")
         soup = BeautifulSoup(result.text, "html.parser")
         results = soup.find_all("div", {"class": "-job"})
         for result in results:
@@ -41,7 +39,8 @@ def extract_jobs(last_page):
     return jobs
 
 
-def get_jobs():
-    last_page = get_last_page()
-    jobs = extract_jobs(last_page)
+def get_jobs(word):
+    url = f"https://stackoverflow.com/jobs?q={word}"
+    last_page = get_last_page(url)
+    jobs = extract_jobs(last_page, url)
     return jobs
