8fad4799b81a4aa38e63dee39764a7dd3fd74364
pyw0214 <yongwookp@gmail.com>
2021-01-05 20:22:24 +0900

    updated_210105

--- main2.py
+++ main2.py
@@ -1,6 +1,7 @@
-from flask import Flask, render_template, request, redirect
+from flask import Flask, render_template, request, redirect, send_file
 from flask.templating import render_template
 from so import get_jobs
+from exporter import save_to_file
 
 app = Flask("SuperScrapper")
 
@@ -29,6 +30,22 @@ def report():
     return render_template("report.html",
                            searchingBy=word, resultsNumber=len(jobs),
                            jobs=jobs)
+
+
+@app.route("/export")
+def export():
+    try:
+        word = request.args.get('word')
+        if not word:
+            raise Exception()
+        word = word.lower()
+        jobs = db.get(word)
+        if not jobs:
+            raise Exception()
+        save_to_file(jobs)
+        return send_file("jobs.csv")
+    except:
+        return redirect("/")
 
 
 app.run()
